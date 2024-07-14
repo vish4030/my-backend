49d057fedcbb7f60e0f16be3feb76e88d9a1c225
vish4030 <vishwajeetkumarraj06@gmail.com>
2024-07-14 14:44:02 +0530

    update

--- db/db.config.js
+++ db/db.config.js
@@ -1,10 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-
 const url = process.env.MONGO_URL;
-
-
 mongoose.connect(url, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
